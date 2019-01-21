b1b5d8942af7b75d612d9cb652d8e3123552bb18
cxyxzh <396946820@qq.com>
2019-01-21 09:42:48 +0800

    测试删除文件 提交后是否改变

--- xzlx-web-api/src/main/java/com/xzlx/web/api/web/dto/TbCommentTables.java
+++
@@ -1,8 +0,0 @@
-package com.xzlx.web.api.web.dto;
-
-public class  TbCommentTables {
-
-    public static final String Travel="tb_travel_comment";
-    public static final String Hotel="tb_hotel_comment";
-    public static final String Strategy="tb_strategy_comment";
-}
